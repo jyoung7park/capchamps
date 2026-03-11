4f5a27e08a2af17eac42738d76c0867b09892085
Juyoung Park <pjuyoung7@gmail.com>
2026-03-11 23:29:14 +0000

    update

--- srv/product-service.js
+++ srv/product-service.js
@@ -1,8 +1,11 @@
 const cds = require('@sap/cds');
+//const { UPDATE } = require('@sap/cds/lib/ql/cds-ql');
 
 module.exports = cds.service.impl(async function (srv) {
 
     const  { Product } = srv.entities;
+    let orignalstock;
+    let ID;
     //srv.before('READ','orderProduct',async(req) =>{
     //    console.log("<<I am in 'BEFORE' Handler")
     //})
@@ -24,10 +27,29 @@ module.exports = cds.service.impl(async function (srv) {
     })
 
     srv.before('orderProduct',async(req) =>{
-        console.log(req.data)
-        console.log(req.params)
+        console.log(req.data);
+        console.log(req.params);
+        //const result = [{ stock: 300 }];
+        const result = await SELECT `stock`  .from(Product).where({ID:req.params[0]});
+        
+        orignalstock = result[0].stock;
+        ID = req.params[0];
+        console.log("<<Orignal Stock",orignalstock);  
 
-        const result = await SELECT `stock` .from(Product).where({ID:req.params[0]});
-        console.log("<<select query",result);
+
+        if(orignalstock > 500){
+            return req.error({
+                code:'400',
+                message:'Stock should not be greater than 500'
+            })
+        }
+    })
+    srv.on('orderProduct',async(req) =>{
+        let updatestock = req.data.stock + orignalstock
+        console.log("<<updated Stock",updatestock);  
+        const result = await UPDATE(Product).with({stock:updatestock}).where({ID:ID});
+        console.log("<<updated query result ",result); 
+
+        return req.notify("Order Placed Update stock is now ${updatestock}")
     })
 })
